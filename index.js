5f31929837e338635a2ef2c2532f60a98fdcfd57
anhdoan <anhdoan@uvic.ca>
2021-01-26 23:57:40 -0800

    add authentication with google oauth

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStratagy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/key");
+
 const app = express();
 
-// a route handler
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// third arg is the redirect URL after user grants permission
+passport.use(
+  new GoogleStratagy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 // default (development environment) is 5000
 const PORT = process.env.PORT || 5000;
